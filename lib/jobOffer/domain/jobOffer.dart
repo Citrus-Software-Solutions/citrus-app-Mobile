12f9000b4e0a32dd60d24376ce5d38602fffbf7c
Julio <diaz.cesar.julio@gmail.com>
2021-06-25 19:57:19 -0400

    feat - Agrego atributo a entidad JobOffer
    Agrego atributo employer a entidad JobOffer

--- lib/jobOffer/domain/jobOffer.dart
+++ lib/jobOffer/domain/jobOffer.dart
@@ -1,13 +1,15 @@
 import 'package:citrus_app_mobile/jobOffer/domain/values/values.dart';
+import 'package:citrus_app_mobile/employer/domain/employer.dart';
 
 class JobOffer {
   final OfferId id;
+  final Employer emloyer;
   final OfferName name;
   final OfferDescription description;
   final OfferGender gender;
   final OfferDateRange dateRange;
   final OfferAgeRange ageRange;
 
-  JobOffer(this.id, this.name, this.description, this.gender, this.dateRange,
-      this.ageRange);
+  JobOffer(this.id, this.emloyer, this.name, this.description, this.gender,
+      this.dateRange, this.ageRange);
 }
