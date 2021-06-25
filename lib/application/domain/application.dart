fafe09c9aa83f8255a7745cd15e1a044af73ca05
Ana Leticia Ibarra <ana.letty07@gmail.com>
2021-06-25 19:07:07 -0400

    [fix] - Modifica import de jobOffer en Application
    Modifico error de import de clase JobOffer en clase Application

--- lib/application/domain/application.dart
+++ lib/application/domain/application.dart
@@ -1,6 +1,6 @@
 import 'package:citrus_app_mobile/application/domain/values/values.dart';
 import 'package:citrus_app_mobile/employee/domain/employee.dart';
-import 'package:citrus_app_mobile/jobOffer/domain/JobOffer.dart';
+import 'package:citrus_app_mobile/jobOffer/domain/jobOffer.dart';
 
 class Application {
   final ApplicationId id;
