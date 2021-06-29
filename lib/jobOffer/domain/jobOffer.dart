80c2ab17014fe2d54b71cb0558cd64c384d85718
Julio <diaz.cesar.julio@gmail.com>
2021-06-28 20:34:43 -0400

    fix - Arreglo typo en atributo de JobOffer
    Arreglo typo en atributo employer de JobOffer

--- lib/jobOffer/domain/jobOffer.dart
+++ lib/jobOffer/domain/jobOffer.dart
@@ -3,13 +3,13 @@ import 'package:citrus_app_mobile/employer/domain/employer.dart';
 
 class JobOffer {
   final OfferId id;
-  final Employer emloyer;
+  final Employer employer;
   final OfferName name;
   final OfferDescription description;
   final OfferGender gender;
   final OfferDateRange dateRange;
   final OfferAgeRange ageRange;
 
-  JobOffer(this.id, this.emloyer, this.name, this.description, this.gender,
+  JobOffer(this.id, this.employer, this.name, this.description, this.gender,
       this.dateRange, this.ageRange);
 }
