87dd8da64f2637b503e7a24ee05539eae9709f6b
Tomas <tomasg92@gmail.com>
2021-06-29 18:44:35 -0400

    [feat] - Agrega archivos relacionados a application
    Agrega use cases, ports in/out, adapters y repositorios para application, para cumplir con arquitectura hexagonal

---
+++ lib/application/application/port/out/createApplicationPort.dart
@@ -0,0 +1,7 @@
+import 'package:citrus_app_mobile/application/domain/application.dart';
+import 'package:citrus_app_mobile/jobOffer/domain/values/offerId.dart';
+import 'package:citrus_app_mobile/user/values/userId.dart';
+
+abstract class CreateApplicationPort {
+  Future<Application?> createApplication(OfferId offerId, UserId employeeId);
+}
--- lib/jobOffer/adapter/out/jobOfferRepository.dart
+++ lib/jobOffer/adapter/out/jobOfferRepository.dart
@@ -1,5 +1,4 @@
 import 'package:citrus_app_mobile/jobOffer/adapter/out/jobOfferMapper.dart';
-import 'package:citrus_app_mobile/user/values/values.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:citrus_app_mobile/jobOffer/domain/jobOffer.dart';
@@ -30,22 +29,5 @@ abstract class JobOfferRepository {
       allJobOffers.add(JobOfferMapper.mapToDomainEntityFromJson(jobOffer));
     }
     return allJobOffers;
-  }
-
-  void applyToJobOffer(OfferId offerId, UserId employeeId) async {
-    final response = await http.post(
-      Uri.parse(apiUrl + 'application'),
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-      body: jsonEncode(<String, dynamic>{
-        'jobOfferId': offerId.getId(),
-        'employeeId': employeeId.getId(),
-      }),
-    );
-
-    if (response.statusCode != 201) {
-      throw Exception('Failed to create the application');
-    }
   }
 }
