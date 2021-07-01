7395c8064e7f4a5382fba4d93e5c4bb861906ddb
Julio <diaz.cesar.julio@gmail.com>
2021-06-30 23:19:46 -0400

    [chore] - Agrego .gitkeep a directorios
    Agrego .gitkeep para mantener la estructura de los directorios

--- util/folder_creator.bat
+++ util/folder_creator.bat
@@ -4,9 +4,22 @@ mkdir %1
 cd %1
 
 mkdir adapter\in
+nul > adapter\in\.gitkeep
+
 mkdir adapter\out
+nul > adapter\out\.gitkeep
+
 mkdir application\port
+nul > application\port\.gitkeep
+
 mkdir application\service
+nul > application\service\.gitkeep
+
 mkdir domain\values
+nul > domain\values\.gitkeep
+
 mkdir ui\screens
+nul > ui\screens\.gitkeep
+
 mkdir ui\widgets
+nul > ui\widgets.gitkeep
