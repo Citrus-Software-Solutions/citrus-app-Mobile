1ca1b0602cbe51052cc4ecfc7a3db26729f4d4f0
Ana Leticia Ibarra <ana.letty07@gmail.com>
2021-07-02 11:57:11 -0400

    [refactor] - Implementación de repositorio para Spring y Mock API.
    Implementación de repositorio para Spring y Mock API para Application

--- lib/application/adapter/out/applicationRepository.dart
+++ lib/application/adapter/out/applicationRepository.dart
@@ -1,30 +1,7 @@
 import 'package:citrus_app_mobile/application/domain/application.dart';
-import 'package:citrus_app_mobile/user/values/values.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 import 'package:citrus_app_mobile/jobOffer/domain/values/offerId.dart';
+import 'package:citrus_app_mobile/user/values/values.dart';
 
 abstract class ApplicationRepository {
-  final String apiUrl =
-      "https://my-json-server.typicode.com/analetty/pruebatipfy/";
-
-  Future<Application?> applyToJobOffer(
-      OfferId offerId, UserId employeeId) async {
-    final response = await http.post(
-      Uri.parse(apiUrl + 'application'),
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-      body: jsonEncode(<String, dynamic>{
-        'jobOfferId': offerId.getId,
-        'employeeId': employeeId.getId,
-      }),
-    );
-
-    if (response.statusCode != 201) {
-      throw Exception('Failed to create the application');
-    }
-
-    return null;
-  }
+  Future<Application?> applyToJobOffer(OfferId offerId, UserId employeeId);
 }
