1595ed3bb6e19991faa7d8a8e819cf9dc90dbea3
Ana Leticia Ibarra <ana.letty07@gmail.com>
2021-07-01 21:42:34 -0400

    {refactor] - Agrego value objects faltantes para entidad JobOffer.
    Agrego value objects faltantes para entidad JobOffer. Agrego salario, ubicación, vacantes y status de oferta de trabajo.

--- lib/jobOffer/domain/jobOffer.dart
+++ lib/jobOffer/domain/jobOffer.dart
@@ -1,15 +1,79 @@
+import 'package:citrus_app_mobile/jobOffer/domain/values/offerVacants.dart';
 import 'package:citrus_app_mobile/jobOffer/domain/values/values.dart';
 import 'package:citrus_app_mobile/employer/domain/employer.dart';
 
 class JobOffer {
-  final OfferId id;
-  final Employer employer;
-  final OfferName name;
-  final OfferDescription description;
-  final OfferGender gender;
-  final OfferDateRange dateRange;
-  final OfferAgeRange ageRange;
+  final OfferId _id;
+  final Employer _employer;
+  final OfferName _name;
+  final OfferDescription _description;
+  final OfferStatus _status;
+  final OfferGender _gender;
+  final OfferSalary _salary;
+  final OfferVacants _vacants;
+  final OfferDateRange _dateRange;
+  final OfferAgeRange _ageRange;
+  final OfferLocation _location;
 
-  JobOffer(this.id, this.employer, this.name, this.description, this.gender,
-      this.dateRange, this.ageRange);
+  JobOffer(
+      this._id,
+      this._employer,
+      this._name,
+      this._description,
+      this._status,
+      this._gender,
+      this._salary,
+      this._vacants,
+      this._dateRange,
+      this._ageRange,
+      this._location);
+
+  OfferId get getId => this._id;
+
+  int? get getIdToInt => this._id.getId;
+
+  String get getIdToString => this._id.getIdToString;
+
+  Employer get getEmployer => this._employer;
+
+  OfferName get getName => this._name;
+
+  String get getNameToString => this._name.getName.toString();
+
+  OfferDescription get getDescription => this._description;
+
+  String get getDescriptionToString =>
+      this._description.getDescription.toString();
+
+  OfferStatus get getStatus => this._status;
+
+  String get getStatusToString => this._status.getStatusToString;
+
+  OfferSalary get getSalary => this._salary;
+
+  String get getSalaryToString => this._salary.getSalaryToString;
+
+  OfferVacants get getVacants => this._vacants;
+
+  String get getVacantsToString => this._vacants.getVacantsToString;
+
+  OfferGender get getGender => this._gender;
+
+  String get getGenderToString => this._gender.getName.toString();
+
+  OfferDateRange get getDateRange => this._dateRange;
+
+  DateTime? get getBeginDate => this._dateRange.getBeginDate;
+
+  DateTime? get getEndDate => this._dateRange.getEndDate;
+
+  OfferAgeRange get getAgeRange => this._ageRange;
+
+  int? get getMinAge => this._ageRange.getminAge;
+
+  int? get getMaxAge => this._ageRange.getMaxAge;
+
+  OfferLocation get getLocation => this._location;
+
+  String get getLocationToString => this._location.getLocation;
 }
---
+++ lib/jobOffer/domain/values/offerLocation.dart
@@ -0,0 +1,10 @@
+class OfferLocation {
+  final String _location;
+  final int _id;
+
+  OfferLocation(this._id, this._location);
+
+  String get getLocation => this._location;
+
+  int get getId => this._id;
+}
