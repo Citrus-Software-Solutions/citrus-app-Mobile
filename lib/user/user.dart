c30a6db2166df76c9a2e4089bee1cdb224ea78d0
Ana Leticia Ibarra <ana.letty07@gmail.com>
2021-07-01 21:51:29 -0400

    [refactor] - Modifico atributos de entidad User.
     Modifico atributos de entidad User. Elimino atributos innecesarios temporalmente.

--- lib/user/user.dart
+++ lib/user/user.dart
@@ -1,9 +1,7 @@
 import 'package:citrus_app_mobile/user/values/values.dart';
 
 class User {
-  final UserAuth auth;
   final UserId id;
-  final UserLocation location;
 
-  User(this.auth, this.id, this.location);
+  User(this.id);
 }
