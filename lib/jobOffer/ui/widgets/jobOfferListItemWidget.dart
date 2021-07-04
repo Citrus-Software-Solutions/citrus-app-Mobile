f5ffbf6a2ce7d8ececbca95b14b1849d64a74074
Ana Leticia Ibarra <ana.letty07@gmail.com>
2021-07-03 22:34:06 -0400

    [feat] - Agrego pruebas de integración flujo de la app.
    Agrego pruebas de integración flujo de la app. Prueba de flujo principal de la app, vista de ofertas de trabajo, click en detalles y en aplicar a oferta de trabajo.
    
    Co-authored-by: Julio Díaz <diaz.cesar.julio@gmail.com>
    Co-authored-by: Tomás Guzmán <tomasg92@gmail.com>

--- lib/jobOffer/ui/widgets/jobOfferListItemWidget.dart
+++ lib/jobOffer/ui/widgets/jobOfferListItemWidget.dart
@@ -5,8 +5,10 @@ import 'package:citrus_app_mobile/jobOffer/domain/jobOffer.dart';
 
 class JobOfferListItemWidget extends StatelessWidget {
   final JobOffer jobOffer;
+  final int index;
 
-  const JobOfferListItemWidget({Key? key, required this.jobOffer})
+  const JobOfferListItemWidget(
+      {Key? key, required this.jobOffer, required this.index})
       : super(key: key);
 
   @override
@@ -14,39 +16,42 @@ class JobOfferListItemWidget extends StatelessWidget {
     try {
       return Card(
         child: ListTile(
-            onTap: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => JobOfferDetailScreen(
-                            title: 'CitrusAPP',
-                            offerId: jobOffer.getId,
-                          )));
-            },
-            title: Padding(
-                padding: EdgeInsets.only(top: 8.0),
-                child: Text(
-                  jobOffer.getNameToString.toUpperCase(),
-                  style: TextStyle(
-                      fontWeight: FontWeight.w800,
-                      color: Theme.of(context).colorScheme.secondary),
-                )),
-            subtitle: Padding(
-                padding: EdgeInsets.symmetric(vertical: 6.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    Text('Compañía: ' + jobOffer.getEmployer.getNameToString),
-                    SizedBox(height: 5),
-                    Text('Ubicación: ' + jobOffer.getLocationToString),
-                  ],
-                )),
-            leading: CircleAvatar(
-                radius: 30,
-                child: ClipOval(
-                  child: Image.asset('util/img/citrus-logo.JPG'),
-                ))),
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => JobOfferDetailScreen(
+                          title: 'CitrusAPP',
+                          offerId: jobOffer.getId,
+                        )));
+          },
+          title: Padding(
+              padding: EdgeInsets.only(top: 8.0),
+              child: Text(
+                jobOffer.getNameToString.toUpperCase(),
+                style: TextStyle(
+                    fontWeight: FontWeight.w800,
+                    color: Theme.of(context).colorScheme.secondary),
+                key: Key('oferta' + index.toString()),
+              )),
+          subtitle: Padding(
+              padding: EdgeInsets.symmetric(vertical: 6.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Text('Compañía: ' + jobOffer.getEmployer.getNameToString),
+                  SizedBox(height: 5),
+                  Text('Ubicación: ' + jobOffer.getLocationToString),
+                ],
+              )),
+          leading: CircleAvatar(
+              radius: 30,
+              child: ClipOval(
+                child: Image.asset('util/img/citrus-logo.JPG'),
+              )),
+          key: Key('offerCard' + index.toString()),
+        ),
       );
     } catch (exception) {
       // TODO: Implementar manejo de errores
