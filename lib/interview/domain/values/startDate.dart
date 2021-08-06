6cc70fe45f34b6b51c55e716c9ea3eafe5d740b0
Ana Leticia Ibarra <ana.letty07@gmail.com>
2021-08-06 10:01:23 -0400

    Agrego titulo en card de detalle de entrevista

--- lib/interview/domain/values/startDate.dart
+++ lib/interview/domain/values/startDate.dart
@@ -4,4 +4,8 @@ class StartDate {
   StartDate(this._start);
 
   DateTime get date => _start;
+
+  String get dateAsString => _start.toLocal().toString().split(" ")[0];
+
+  String get timeAsString => _start.toLocal().toString().split(" ")[1];
 }
